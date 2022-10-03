b31071657bcea0b973bd3cb8a7c6a6800c4c481c
khshanovskyi <khshanovskyi@gmail.com>
2022-10-03 20:37:27 +0300

    Initial commit

---
+++ src/main/java/ua/khshanovskyi/task/CreditAdvisoryTaskApplication.java
@@ -0,0 +1,13 @@
+package ua.khshanovskyi.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditAdvisoryTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditAdvisoryTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/khshanovskyi/task/CreditAdvisoryTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.khshanovskyi.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditAdvisoryTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
